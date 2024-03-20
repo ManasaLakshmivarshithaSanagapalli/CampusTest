51af6a0c912b1045fd96199b62af8d41238f3e62
ManasaLakshmivarshithaSanagapalli <97424517+ManasaLakshmivarshithaSanagapalli@users.noreply.github.com>
2024-03-20 11:07:04 +0530

    hai
    Hai

---
+++ Lexa_React_v2.0.0/Admin/src/components/apii/SentimentalAnalysis.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import './apii.css'
+
+const AzureSentimentAnalysis = () => {
+    const [text, setText] = useState('');
+    const [sentiment, setSentiment] = useState(null);
+
+    const analyzeSentiment = async () => {
+        try {
+            const response = await axios.post(
+                'https://eastus.api.cognitive.microsoft.com/text/analytics/v3.0/sentiment',
+                {
+                    documents: [
+                        {
+                            id: '1',
+                            text: text,
+                        },
+                    ],
+                },
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Ocp-Apim-Subscription-Key': '9f54ecd90376405587302221386854f8',
+                    },
+                }
+            );
+            console.log('Sentiment:', response.data.documents[0]);
+            setSentiment(response.data.documents[0].sentiment);
+        } catch (error) {
+            console.error('Error analyzing sentiment:', error);
+        }
+    };
+
+    return (
+        <div className="container">
+            <h2>Sentiment Analysis</h2>
+            <textarea
+                className="textarea"
+                rows="4"
+                cols="50"
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+                placeholder="Enter text for sentiment analysis..."
+            ></textarea>
+            <br />
+            <button className="button" onClick={analyzeSentiment}>
+                Analyze Sentiment
+            </button>
+            {sentiment && (
+                <div className="sentiment">
+                    <h3>Sentiment: {sentiment}</h3>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default AzureSentimentAnalysis;
